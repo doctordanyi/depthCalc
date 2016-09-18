397814850b72ec7566efd33e3116cc8b822f77de
David <david.danyi92@gmail.com>
2016-09-18 21:29:47 +0200

    Parameter container class added

---
+++ depthCalc/ParamContainer.cs
@@ -0,0 +1,24 @@
+﻿using Emgu.CV.CvEnum;
+using System;
+using System.Collections.Generic;
+
+namespace depthCalc
+{
+    class ParamContainer
+    {
+        // Store parameters selected by the user
+
+        // File paths
+        public String pathReference;
+        public String pathData;
+        public String pathResult;
+
+        // Preprocessing parameters
+        public int scale;
+
+        // Processing parameters
+        TemplateMatchingType matchMethod;
+
+        // Postprocessing params
+    }
+}
