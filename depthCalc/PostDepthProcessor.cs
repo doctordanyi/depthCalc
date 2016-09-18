8968bc240a18b3798d0a50ab752996cab5decdf6
doctordanyi <david.danyi92@gmail.com>
2016-09-18 23:25:26 +0200

    Started working on pre-postprocessing classes

--- depthCalc/PostDepthProcessor.cs
+++ depthCalc/PostDepthProcessor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Emgu.CV;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,15 @@ namespace depthCalc
 {
     class PostDepthProcessor
     {
+        // Supported postprocessing steps
+        public enum SupportedSteps
+        {
+            MedianFilter
+        };
+
+        public void MedianFilter(int kernelSize, out Mat Data)
+        {
+            Data = new Mat();
+        }
     }
 }
--- depthCalc/PreDepthProcessor.cs
+++ depthCalc/PreDepthProcessor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Emgu.CV;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,28 @@ namespace depthCalc
 {
     class PreDepthProcessor
     {
+        // Supported preprocessing steps
+        public enum SupportedSteps
+        {
+            Scale,
+            GaussianBlur,
+            DifferenceOfGaussians
+        };
+
+        public void Scale(int scaleDownFactorm, out Mat Data)
+        {
+            Data = new Mat();
+        }
+
+        public void GaussianBlur(int kernelSize, float sigma, out Mat Data)
+        {
+            Data = new Mat();
+        }
+
+        public void DifferenceOfGaussians(int kernelSize1, int kernelSize2, out Mat Data)
+        {
+            Data = new Mat();
+        }
+
     }
 }
