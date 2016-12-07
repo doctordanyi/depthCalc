6b9d0c473d83e76ca06d09bcd88d45c39767489b
doctordanyi <david.danyi92@gmail.com>
2016-12-07 09:12:27 +0100

    Added class for clustering local optimization step

---
+++ depthCalc/Processing/Depthprocessing/ClusteringMatchSelection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Emgu.CV;
+using System.Drawing;
+
+namespace DepthCalc.Processing.Depthprocessing
+{
+    class ClusteringMatchSelection : ProcessingStep
+    {
+        private Rectangle windowArea;
+
+        public override Mat doYourJob()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
